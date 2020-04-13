88beb469a58911817b4d4c0b98e3c554d57569f4
sharanaru <sharan.arumugam@colorado.edu>
2020-04-12 21:20:41 -0600

    Init scripts to load modules

--- base_external/post-build.sh
+++ base_external/post-build.sh
@@ -9,8 +9,8 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
-cp /home/sharan/Documents/aesd-project/buildroot-assignments-base/base_external/interfaces ${TARGET_DIR}/etc/network/interfaces
-cp /home/sharan/Documents/aesd-project/buildroot-assignments-base/base_external/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
+cp ../base_external/interfaces ${TARGET_DIR}/etc/network/interfaces
+cp ../base_external/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
 
 
 
