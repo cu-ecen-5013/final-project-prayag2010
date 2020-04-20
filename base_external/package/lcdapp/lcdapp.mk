0984e874934cf5213582727692abe8fd22ddd94f
sharanaru <sharan.arumugam@colorado.edu>
2020-04-19 21:09:17 -0600

    added server package

--- base_external/package/lcdapp/lcdapp.mk
+++ base_external/package/lcdapp/lcdapp.mk
@@ -6,18 +6,20 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
-AESD_ASSIGNMENTS_SITE_METHOD = git
+LCDAPP_VERSION = f5299684edfcb625dbe130a9da374182fe723816
+LCDAPP_SITE = git@github.com:cu-ecen-5013/final-project-sharanaru.git
+LCDAPP_SITE_METHOD = git
 
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
+define LCDAPP_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
 #TODO: Add required executables or scripts below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
+define LCDAPP_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/client $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/controlscript $(TARGET_DIR)/bin
 endef
 
 
--- base_external/post-build.sh
+++ base_external/post-build.sh
@@ -9,6 +9,9 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
+chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
+cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
 cp ../base_external/interfaces ${TARGET_DIR}/etc/network/interfaces
 cp ../base_external/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
 cp ../base_external/spiOverlay.dtbo ${TARGET_DIR}/../images/rpi-firmware/overlays/spiOverlay.dtbo
