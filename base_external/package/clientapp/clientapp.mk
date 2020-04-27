e91275f5ecbf065ab7353aefba28871c42c45a33
sharanaru <sharan.arumugam@colorado.edu>
2020-04-27 10:10:29 -0600

    Added a Client package and CHanges to controlscript

--- base_external/package/clientapp/clientapp.mk
+++ base_external/package/clientapp/clientapp.mk
@@ -6,19 +6,18 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-SCAPP_VERSION =  8d25186485c8ac10774e5e184503e54e62eb9b65
-SCAPP_SITE = git@github.com:cu-ecen-5013/final-project-sharanaru.git
-SCAPP_SITE_METHOD = git
+CLIENTAPP_VERSION =  5f55fe41dcb10b534262fba2699f0e5125d31f20
+CLIENTAPP_SITE = git@github.com:cu-ecen-5013/final-project-rahulramaprasad7.git
+CLIENTAPP_SITE_METHOD = git
 
 
-define SCAPP_BUILD_CMDS
+define CLIENTAPP_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
 #TODO: Add required executables or scripts below
-define SCAPP_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/controlscript $(TARGET_DIR)/bin
+define CLIENTAPP_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/client $(TARGET_DIR)/bin
 	
 endef
 
